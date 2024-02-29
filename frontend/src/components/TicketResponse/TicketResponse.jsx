0b7c68114e808e02148f24226fa5486af8af5f63
Tyler washington <lilturtleneckbeats@gmail.com>
2024-02-28 22:27:08 -0500

    added class names to ticketResponse component

--- frontend/src/components/TicketResponse/TicketResponse.jsx
+++ frontend/src/components/TicketResponse/TicketResponse.jsx
@@ -3,12 +3,12 @@ import './TicketResponse.css'
 
 export default function TicketResponse({ticketResponse}) {
   return (
-    <div>
-        <div>
-            <div >Admin</div>
-            <div>{ticketResponse.status}</div>
+    <div className='ticket-res-ctn'>
+        <div className='ticket-res-admin-status'>
+            <div className='ticket-res-name'>Admin</div>
+            <div className='ticket-res-status'>{ticketResponse.status}</div>
         </div>
-        <div>{ticketResponse.response}</div>
+        <div className='ticket-res-response'>{ticketResponse.response}</div>
     </div>
   )
 }
