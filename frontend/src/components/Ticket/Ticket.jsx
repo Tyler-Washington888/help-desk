5a66fa076751f1eaecc4bba848ed894f2a08bf8b
Tyler washington <lilturtleneckbeats@gmail.com>
2024-02-28 23:34:44 -0500

    pushing to heroku

--- frontend/src/components/Ticket/Ticket.jsx
+++ frontend/src/components/Ticket/Ticket.jsx
@@ -20,7 +20,7 @@ export default function Ticket({index, tickets, setTickets }) {
       };
   
       fetchTicketResponses();
-    }, [ticketResponses, tickets, index ]);
+    }, [ticketResponses, tickets, index]);
 
     function toggleReponseForm() {
         setResponseFormOpen(!responseFormOpen)
