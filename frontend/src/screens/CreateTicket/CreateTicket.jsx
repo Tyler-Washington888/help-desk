bc6f094be32fc45bcb5a7f5dcd81bf9d213d0f11
Tyler washington <lilturtleneckbeats@gmail.com>
2024-02-28 22:47:03 -0500

    added class names to elements within create ticket screen

--- frontend/src/screens/CreateTicket/CreateTicket.jsx
+++ frontend/src/screens/CreateTicket/CreateTicket.jsx
@@ -6,11 +6,11 @@ import { NavLink } from "react-router-dom";
 export default function CreateTicket() {
 
     return (
-        <div>
-            <button>
-                <NavLink to='/adminPanel'><h1>Admin Panel</h1></NavLink>
+        <div className='create-ticket-ctn '>
+            <button className='admin-panel-ctn'>
+                <NavLink className="admin-panel-btn" to='/adminPanel'><h1>Admin Panel</h1></NavLink>
             </button>
-            <div>
+            <div className='new-ticket-ctn'>
                 <h1>Submit Request</h1>
                 <NewTicketForm></NewTicketForm>
             </div>
