aa4f7599a1f40d58be337f5bc6479952c315b66a
Tyler washington <lilturtleneckbeats@gmail.com>
2024-02-28 21:10:52 -0500

    added useEffect hook to get tickets in root

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,26 @@
 import './App.css';
 import { Route, Routes } from "react-router";
 import { useState, useEffect } from "react";
+import { getTickets } from "./services/tickets.js";
 
 
 function App() {
+  const [tickets, setTickets] = useState(null);
+
+  useEffect(() => {
+    const fetchTickets  = async () => {
+
+      let tickets = await getTickets();
+
+      if (tickets) {
+        setTickets(tickets);
+      }
+    };
+
+    fetchTickets();
+  }, [tickets]);
+
+
   return (
     <div>
     </div>
