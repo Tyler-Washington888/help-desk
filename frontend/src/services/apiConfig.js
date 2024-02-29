e63ed5af03d10bcafa288851750fabb9f2f74601
Tyler washington <lilturtleneckbeats@gmail.com>
2024-02-29 01:20:49 -0500

    editing basurl

--- frontend/src/services/apiConfig.js
+++ frontend/src/services/apiConfig.js
@@ -5,7 +5,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL:
     process.env.NODE_ENV === "production"
-      ? ""
+      ? "https://help-desk-2024-c0fbdd12049a.herokuapp.com/"
       : "http://localhost:8000",
 });
 
